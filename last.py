84c4d4f121b97e8e891edda416d117bd306b9fca
mrrocky <mrrocky@mrpc>
2021-12-14 13:43:36 -0500

    comit

--- last.py
+++ last.py
@@ -1,10 +1,27 @@
 import os
-#fdsf
+import shutil
 f=open('./config.pgit','r')
 contenido=f.readline()
 repository=contenido.split('/')[-1].split('.')[0]
 link=contenido.split('=')[1]
 #sdf
-os.chdir('..')
-os.system(f'sudo rm -r {repository}')
+files=os.listdir('.')
+for file in files:
+    try:
+        os.remove(file)
+    except:
+        try:
+            os.rmdir(file)
+        except:
+             shutil.rmtree(file)
+
+
 os.system(f'git clone {link}')
+os.chdir(repository)
+
+file2 =os.listdir('.')
+for file in file2:
+    shutil.move(file, '../')
+os.chdir('../')
+shutil.rmtree(repository)
+
