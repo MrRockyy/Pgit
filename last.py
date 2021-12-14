e6a6003cc02028d45e1862a0373b9f8b88ddeadb
mrrocky <mrrocky@mrpc>
2021-12-13 22:52:50 -0500

    comit

--- last.py
+++ last.py
@@ -1,9 +1,10 @@
-import os 
-f=open('./config.pgit','rb')
+import os
+#fdsf
+f=open('./config.pgit','r')
 contenido=f.readline()
 repository=contenido.split('/')[-1].split('.')[0]
 link=contenido.split('=')[1]
 #sdf
 os.chdir('..')
-os.system(f'rm -r {repository}')
-os.system('git clone {link}')
+os.system(f'sudo rm -r {repository}')
+os.system(f'git clone {link}')
