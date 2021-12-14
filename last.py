b80fe18bf79e53ddf28093fd1a5346bf526357b2
mrrocky <mrrocky@mrpc>
2021-12-13 23:39:48 -0500

    comit

--- last.py
+++ last.py
@@ -5,7 +5,6 @@ contenido=f.readline()
 repository=contenido.split('/')[-1].split('.')[0]
 link=contenido.split('=')[1]
 #sdf
-os.system('source ~/.config/pgit/cd.sh ..')
+os.chdir('..')
 os.system(f'sudo rm -r {repository}')
 os.system(f'git clone {link}')
-os.system(f'source ~/.config/pgit/cd.sh {repository}')
---
+++ upload.py
@@ -0,0 +1,5 @@
+import os 
+
+os.system("git add .; git commit -m 'comit';git push -u origin master") 
+
+uploaded 
