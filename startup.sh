3b2390c10502740cf5bd41ca97774aef464b1578
mrrocky <mrrocky@mrpc>
2021-12-22 11:07:20 -0500

    comit

--- startup.sh
+++ startup.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
 mkdir -p ~/.config
-rm -r ~/.config/pgit
 cd ..
 mv pgit ~/.config 
-cd /.config/pgit
+cd ~/.config/pgit
 chmod +x pgit
-rm -r /usr/bin/pgit
-sudo mv pgit /usr/bin/
+sudo cp pgit /usr/bin/pgit
 
 
 
